9c2574d4ef757a7404b3ee591966c454d0f71a30
rownpoolleague <rownpoolleague@gmail.com>
2026-02-05 11:09:09 -0600

    Create schedule.js

---
+++ schedule.js
@@ -0,0 +1,18 @@
+<tbody>
+  <tr><td>1</td><td>1/20/2026</td><td>1v10(4-1)</td><td>2v9(2-3)</td><td>3v8(5-0)</td><td>7v4(1-4)</td><td>5v6(2-3)</td></tr>
+  <tr><td>2</td><td>1/27/2026</td><td>10v2(3-2)</td><td>3v1(2-3)</td><td>4v9(3-2)</td><td>8v5(3-2)</td><td>6v7(3-2)</td></tr>
+  <tr><td>3</td><td>2/3/2026</td><td>10v3(-)</td><td>2v4(-)</td><td>5v1(-)</td><td>9v6(-)</td><td>7v8(-)</td></tr>
+  <tr><td>4</td><td>2/10/2026</td><td>4v10(-)</td><td>3v5(-)</td><td>6v2(-)</td><td>1v7(-)</td><td>8v9(-)</td></tr>
+  <tr><td>5</td><td>2/17/2026</td><td>5v10(-)</td><td>6v4(-)</td><td>7v3(-)</td><td>8v2(-)</td><td>9v1(-)</td></tr>
+  <tr><td>6</td><td>2/24/2026</td><td>10v6(-)</td><td>7v5(-)</td><td>4v8(-)</td><td>3v9(-)</td><td>1v2(-)</td></tr>
+  <tr><td>7</td><td>3/3/2026</td><td>10v7(-)</td><td>6v8(-)</td><td>9v5(-)</td><td>1v4(-)</td><td>2v3(-)</td></tr>
+  <tr><td>8</td><td>3/10/2026</td><td>8v10(-)</td><td>7v9(-)</td><td>1v6(-)</td><td>5v2(-)</td><td>4v3(-)</td></tr>
+  <tr><td>9</td><td>3/17/2026</td><td>9v10(-)</td><td>8v1(-)</td><td>2v7(-)</td><td>3v6(-)</td><td>5v4(-)</td></tr>
+  <tr><td>10</td><td>3/24/2026</td><td>10v1(-)</td><td>9v2(-)</td><td>3v8(-)</td><td>4v7(-)</td><td>6v5(-)</td></tr>
+  <tr><td>11</td><td>3/31/2026</td><td>2v10(-)</td><td>1v3(-)</td><td>9v4(-)</td><td>5v8(-)</td><td>6v7(-)</td></tr>
+  <tr><td>12</td><td>4/7/2026</td><td>3v10(-)</td><td>4v2(-)</td><td>5v1(-)</td><td>6v9(-)</td><td>8v7(-)</td></tr>
+  <tr><td>13</td><td>4/14/2026</td><td>10v4(-)</td><td>3v5(-)</td><td>2v6(-)</td><td>7v1(-)</td><td>9v8(-)</td></tr>
+  <tr><td>14</td><td>4/21/2026</td><td>5v10(-)</td><td>4v6(-)</td><td>7v3(-)</td><td>8v2(-)</td><td>1v9(-)</td></tr>
+  <tr><td>15</td><td>4/28/2026</td><td>10v6(-)</td><td>7v5(-)</td><td>8v4(-)</td><td>9v3(-)</td><td>2v1(-)</td></tr>
+  <tr><td>16</td><td>5/5/2026</td><td colspan=5></td></tr>
+</tbody>
