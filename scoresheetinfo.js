d11d4681b683b9f63e27f9251322310fbefd6703
rownpoolleague <rownpoolleague@gmail.com>
2026-02-06 00:27:36 -0600

    Update scoresheetinfo.js

--- scoresheetinfo.js
+++ scoresheetinfo.js
@@ -6,7 +6,7 @@ const scoresheetinfo = {
     "Dirty Ballers":["Rocky Oiler Sr.","Kevin Pattenaude","Ron Gonzalez","Javier Mauricio","Peggy Wood","Joey Sanchez","John Schoiber","Chuck Feager"],
     "8 on Break":["Doc Halek","Juergen Jackert","Danny Plowman","John Amrein","Kevin Ward","Shawn Mcbride","Don Murphy","Kenny Esposito"],
     "Five Amigos":["Francisco Tinajero","Sergio Sanchez","Mario Orenday","Felipe Ochoa","Jeronimo Ramirez","Kevin Joyner","Tony Perez"],
-    "Frank's Pizza":["Jose Gonzalez","Luis Asavedo","Rico Urvina","Jariel Diaz*","Miguel Arias"],
-    "Killer Instinct":["Mike Uhlean","Doug Andrus*","Jim Berger","Shawn Vitolka*","Mike Zbonski Jr.","Luke Valenzuela","Samantha Kommenich"],
+    "Frank's Pizza":["Jose Gonzalez","Luis Asavedo","Rico Urvina","Jariel Diaz","Miguel Arias"],
+    "Killer Instinct":["Mike Uhlean","Doug Andrus","Jim Berger","Shawn Vitolka","Mike Zbonski Jr.","Luke Valenzuela","Samantha Kommenich"],
     "Pete's Bunch":["Tom Schroeder","Chuck Harding","Dan Schroeder","Rob Zwart","Pete Steele","Buster Fuller","Ron Podborny","Tony Castro"]
 };
