c655dd6db8a9be5c8e6f3c3c158c1093860d6e84
rownpoolleague <rownpoolleague@gmail.com>
2026-02-10 20:34:49 -0600

    Update scoresheetinfo.js

--- scoresheetinfo.js
+++ scoresheetinfo.js
@@ -3,7 +3,7 @@ const scoresheetinfo = {
     "Haters Welcome":["Randy Rycraft","Joe Neal","Jennifer Wagner","Jim Czenk","Phil Nelson","Julie Ringbauer","Mike Gore","Brett Boger"],
     "Lynn's Amigos":["Francisco Ibarra","Noe Castana","Enrique Meza","Tomas Ramirez"],
     "Rackless Behavior":["Jim Nevin","Larry Casper","Harnaik Lal","Freddy Izguerra","Tom Nevin","Doug Misner","Joe Crutchfield","Ross Giannino"],
-    "Dirty Ballers":["Rocky Oiler Sr.","Kevin Pattenaude","Ron Gonzalez","Javier Mauricio","Peggy Wood","Joey Sanchez","John Schoiber","Chuck Feager"],
+    "Dirty Ballers":["Rocky Oiler Sr.","Kevin Pattenaude","Greg Wood","Javier Mauricio","Peggy Wood","Joey Sanchez","John Schoiber","Chuck Feager"],
     "8 on Break":["Doc Halek","Juergen Jackert","Danny Plowman","John Amrein","Kevin Ward","Shawn Mcbride","Don Murphy","Kenny Esposito"],
     "Five Amigos":["Francisco Tinajero","Sergio Sanchez","Mario Orenday","Felipe Ochoa","Jeronimo Ramirez","Kevin Joyner","Tony Perez"],
     "Frank's Pizza":["Jose Gonzalez","Luis Asavedo","Rico Urvina","Jariel Diaz","Miguel Arias"],
