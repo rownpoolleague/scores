ff3ca5526d598a9f2317f0daf1c9a9eac5133537
rownpoolleague <rownpoolleague@gmail.com>
2026-02-25 18:47:25 -0600

    Automated scoresheet info update

--- scoresheetinfo.js
+++ scoresheetinfo.js
@@ -1,12 +1,12 @@
 const scoresheetinfo = {
-    "Halfway Crooks":["Dan Foster","Tony Perelli","Don Young","Don Rak","Yani Marinopoulos","Brooke Bridges","Jeremie Ketner","Tony Guthrie"],
+    "Halfway Crooks":["Dan Foster","Tony Perelli","Don Young","Don Rak","Yani Marinopoulos","Tony Guthrie","Jeremie Ketner"],
     "Haters Welcome":["Randy Rycraft","Joe Neal","Jennifer Wagner","Jim Czenk","Phil Nelson","Julie Ringbauer","Mike Gore","Brett Boger"],
     "Lynn's Amigos":["Francisco Ibarra","Noe Castana","Enrique Meza","Tomas Ramirez","Ignasio Gutierrez","Jose Munoz"],
     "Rackless Behavior":["Jim Nevin","Larry Casper","Harnaik Lal","Freddy Izguerra","Tom Nevin","Doug Misner","Joe Crutchfield","Ross Giannino"],
     "Dirty Ballers":["Rocky Oiler Sr.","Kevin Pattenaude","Greg Wood","Javier Mauricio","Peggy Wood","Joey Sanchez","John Schoiber","Chuck Feager"],
     "8 on Break":["Doc Halek","Juergen Jackert","Danny Plowman","John Amrein","Kevin Ward","Shawn Mcbride","Don Murphy","Kenny Esposito"],
     "Five Amigos":["Francisco Tinajero","Sergio Sanchez","Mario Orenday","Felipe Ochoa","Jeronimo Ramirez","Kevin Joyner","Tony Perez"],
-    "Frank's Pizza":["Jose Gonzalez","Luis Asavedo","Ricardo Urvina","Jariel Diaz","Miguel Arias"],
+    "Frank's Pizza":["Jose Gonzalez","Luis Asavedo","Ricardo Urvina","Jariel Diaz","Miguel Arias","Jose Silva"],
     "Killer Instinct":["Mike Uhlean","Doug Andrus","Jim Berger","Shawn Vitolka","Mike Zbonski Jr.","Luke Valenzuela","Samantha Kommenich","Alex Cruz Sr."],
     "Pete's Bunch":["Tom Schroeder","Chuck Harding","Dan Schroeder","Rob Zwart","Pete Steele","Buster Fuller","Ron Podborny","Tony Castro"]
 };
