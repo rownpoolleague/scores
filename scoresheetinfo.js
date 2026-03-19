21450ba1908fc72f4555c3affb7f364021e43f71
rownpoolleague <rownpoolleague@gmail.com>
2026-03-19 04:12:22 -0500

    Update scoresheetinfo.js

--- scoresheetinfo.js
+++ scoresheetinfo.js
@@ -1,12 +1,12 @@
 const scoresheetinfo = {
-    "Halfway Crooks":["Dan Foster","Tony Perelli","Don Young","Don Rak","Yani Marinopoulos","Tony Guthrie","Jeremie Ketner"],
-    "Haters Welcome":["Randy Rycraft","Joe Neal","Jennifer Wagner","Jim Czenk","Phil Nelson","Julie Ringbauer","Mike Gore","Brett Boger"],
-    "Lynn's Amigos":["Francisco Ibarra","Noe Castana","Enrique Meza","Tomas Ramirez","Ignasio Gutierrez","Jose Munoz"],
-    "Rackless Behavior":["Jim Nevin","Larry Casper","Harnaik Lal","Freddy Izguerra","Tom Nevin","Doug Misner","Joe Crutchfield","Ross Giannino"],
-    "Dirty Ballers":["Rocky Oiler Sr.","Kevin Pattenaude","Greg Wood","Javier Mauricio","Peggy Wood","Joey Sanchez","John Schoiber","Chuck Feager"],
-    "8 on Break":["Doc Halek","Juergen Jackert","Danny Plowman","John Amrein","Kevin Ward","Shawn Mcbride","Don Murphy","Kenny Esposito"],
-    "Five Amigos":["Francisco Tinajero","Sergio Sanchez","Mario Orenday","Felipe Ochoa","Jeronimo Ramirez","Kevin Joyner","Tony Perez"],
-    "Frank's Pizza":["Jose Gonzalez","Luis Asavedo","Ricardo Urvina","Jariel Diaz","Miguel Arias","Jose Silva"],
-    "Killer Instinct":["Mike Uhlean","Alex Gomez","Jim Berger","Shawn Vitolka","Mike Zbonski Jr.","Luke Valenzuela"],
-    "Pete's Bunch":["Tom Schroeder","Chuck Harding","Dan Schroeder","Rob Zwart","Pete Steele","Buster Fuller","Ron Podborny","Tony Castro"]
+    "1 Halfway Crooks":["Dan Foster","Tony Perelli","Don Young","Don Rak","Yani Marinopoulos","Tony Guthrie","Jeremie Ketner"],
+    "2 Haters Welcome":["Randy Rycraft","Joe Neal","Jennifer Wagner","Jim Czenk","Phil Nelson","Julie Ringbauer","Mike Gore","Brett Boger"],
+    "3 Lynn's Amigos":["Francisco Ibarra","Noe Castana","Enrique Meza","Tomas Ramirez","Ignasio Gutierrez","Jose Munoz"],
+    "4 Rackless Behavior":["Jim Nevin","Larry Casper","Harnaik Lal","Freddy Izguerra","Tom Nevin","Doug Misner","Joe Crutchfield","Ross Giannino"],
+    "5 Dirty Ballers":["Rocky Oiler Sr.","Kevin Pattenaude","Greg Wood","Javier Mauricio","Peggy Wood","Joey Sanchez","John Schoiber","Chuck Feager"],
+    "6 8 on Break":["Doc Halek","Juergen Jackert","Danny Plowman","John Amrein","Kevin Ward","Shawn Mcbride","Don Murphy","Kenny Esposito"],
+    "7 Five Amigos":["Francisco Tinajero","Sergio Sanchez","Mario Orenday","Felipe Ochoa","Jeronimo Ramirez","Kevin Joyner","Tony Perez"],
+    "8 Frank's Pizza":["Jose Gonzalez","Luis Asavedo","Ricardo Urvina","Jariel Diaz","Miguel Arias","Jose Silva"],
+    "9 Killer Instinct":["Mike Uhlean","Alex Gomez","Jim Berger","Shawn Vitolka","Mike Zbonski Jr.","Luke Valenzuela"],
+    "10 Pete's Bunch":["Tom Schroeder","Chuck Harding","Dan Schroeder","Rob Zwart","Pete Steele","Buster Fuller","Ron Podborny","Tony Castro"]
 };
