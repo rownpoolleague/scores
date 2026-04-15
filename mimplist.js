99b34fd5c5483fda28a2c5647f79422ae507e289
rownpoolleague <rownpoolleague@gmail.com>
2026-04-15 14:50:20 -0500

    Automated Mimp leaders update

--- mimplist.js
+++ mimplist.js
@@ -1,12 +1,12 @@
 const mimpList = [
   {"Player Name":"Kevin Pattenaude","Points":"37.27%"},
+  {"Player Name":"Mario Orenday","Points":"36.03%"},
   {"Player Name":"Shawn Mcbride","Points":"32.11%"},
-  {"Player Name":"Mario Orenday","Points":"27.94%"},
   {"Player Name":"Chuck Harding","Points":"27.55%"},
-  {"Player Name":"Don Young","Points":"25.87%"},
-  {"Player Name":"Randy Rycraft","Points":"23.14%"},
-  {"Player Name":"Mike Zbonski Jr.","Points":"19.41%"},
+  {"Player Name":"Mike Zbonski Jr.","Points":"23.63%"},
+  {"Player Name":"Francisco Ibarra","Points":"22.63%"},
+  {"Player Name":"Randy Rycraft","Points":"22.57%"},
+  {"Player Name":"Don Young","Points":"19.90%"},
   {"Player Name":"Phil Nelson","Points":"17.45%"},
-  {"Player Name":"Francisco Ibarra","Points":"16.05%"},
-  {"Player Name":"Luis Asavedo","Points":"14.81%"}
+  {"Player Name":"Doug Andrus","Points":"14.58%"}
 ];
