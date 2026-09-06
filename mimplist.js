59471631d3b6ce34f2b283cc25c623c0b05ff01c
rownpoolleague <rownpoolleague@gmail.com>
2026-09-06 14:48:30 -0500

    Automated update: mimplist.js

--- mimplist.js
+++ mimplist.js
@@ -1,12 +1,2 @@
 const mimpList = [
-  {"Player Name":"Don Young","Points":"50.75%"},
-  {"Player Name":"Kevin Pattenaude","Points":"38.19%"},
-  {"Player Name":"Mario Orenday","Points":"30.88%"},
-  {"Player Name":"Kevin Joyner","Points":"28.49%"},
-  {"Player Name":"Enrique Meza","Points":"24.53%"},
-  {"Player Name":"Francisco Ibarra","Points":"22.63%"},
-  {"Player Name":"Doc Halek","Points":"22.55%"},
-  {"Player Name":"Doug Andrus","Points":"21.88%"},
-  {"Player Name":"Chuck Harding","Points":"21.05%"},
-  {"Player Name":"Randy Rycraft","Points":"14.29%"}
 ];
