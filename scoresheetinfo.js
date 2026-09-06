c2c293a51e2ebf30a8c6f70f5d4698bbfa2574e1
rownpoolleague <rownpoolleague@gmail.com>
2026-09-06 15:14:42 -0500

    Automated update: scoresheetinfo.js

--- scoresheetinfo.js
+++ scoresheetinfo.js
@@ -2,7 +2,7 @@ const scoresheetinfo = {
     "1 Halfway Crooks":["Dan Foster","Tony Perelli","Don Young","Don Rak","Yani Marinopoulos","Tony Guthrie","Jeremie Ketner"],
     "2 Haters Welcome":["Randy Rycraft","Joe Neal","Jennifer Wagner","Jim Czenk","Phil Nelson","Julie Ringbauer","Mike Gore","Brett Boger"],
     "3 Lynn's Amigos":["Francisco Ibarra","Noe Castana","Enrique Meza","Tomas Ramirez","Ignasio Gutierrez","Jose Munoz"],
-    "4 Rackless Behavior":["Jim Nevin","Larry Casper","Harnaik Lal","Freddy Izguerra","Tom Nevin","Doug Misner","Joe Crutchfield","Ross Giannino"],
+    "4 Rackless Behavior":["Jim Bullock","Patti Bullock"],
     "5 Dirty Ballers":["Rocky Oiler Sr.","Kevin Pattenaude","Greg Wood","Javier Mauricio","Peggy Wood","Joey Sanchez","John Schoiber","Chuck Feager"],
     "6 8 on Break":["Doc Halek","Juergen Jackert","Danny Plowman","John Amrein","Kevin Ward","Shawn Mcbride","Don Murphy","Kenny Esposito"],
     "7 Five Amigos":["Francisco Tinajero","Sergio Sanchez","Mario Orenday","Felipe Ochoa","Jeronimo Ramirez","Kevin Joyner","Tony Perez"],
