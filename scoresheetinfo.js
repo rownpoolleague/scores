0423830d3048ede93d9814142a04cd125c6b6fb4
rownpoolleague <rownpoolleague@gmail.com>
2026-09-11 16:55:09 -0500

    Automated update: scoresheetinfo.js

--- scoresheetinfo.js
+++ scoresheetinfo.js
@@ -1,12 +1,12 @@
 const scoresheetinfo = {
-    "1 Halfway Crooks":["Dan Foster","Tony Perelli","Don Young","Don Rak","Yani Marinopoulos","Tony Guthrie","Jeremie Ketner"],
-    "2 Haters Welcome":["Randy Rycraft","Joe Neal","Jennifer Wagner","Jim Czenk","Phil Nelson","Julie Ringbauer","Brett Boger"],
+    "1 Halfway Crooks":["Dan Foster","Tony Perelli","Don Young","Don Rak","Yani Marinopoulos","Tony Guthrie","Jeremie Ketner","Brooke Bridges"],
+    "2 Haters Welcome":["Randy Rycraft","Joe Neal","Jennifer Wagner","Jim Czenk","Phil Nelson","Julie Ringbauer","Brett Boger","Ruth Drozdek"],
     "3 Lynn's Amigos":["Francisco Ibarra","Noe Castana","Enrique Meza","Tomas Ramirez","Ignasio Gutierrez","Jose Munoz"],
-    "4 Only Here For an Alibi":["Jim Bullock","Patti Bullock","Tito Sanchez","Jack Sedivy"],
+    "4 Rackless Behavior":["Jim Bullock","Patti Bullock","Tito Sanchez","Jack Sedivy","Thomas Who","Joey Who"],
     "5 Dirty Ballers":["Rocky Oiler Sr.","Kevin Pattenaude","Greg Wood","Javier Mauricio","Peggy Wood","Joey Sanchez","John Schoiber","Chuck Feager"],
     "6 8 on Break":["Doc Halek","Juergen Jackert","Danny Plowman","John Amrein","Kevin Ward","Shawn Mcbride","Don Murphy","Kenny Esposito"],
     "7 Five Amigos":["Francisco Tinajero","Sergio Sanchez","Mario Orenday","Felipe Ochoa","Jeronimo Ramirez","Kevin Joyner","Tony Perez"],
     "8 Frank's Pizza":["Jose Gonzalez","Luis Asavedo","Ricardo Urvina","Jariel Diaz","Miguel Arias","Jose Silva"],
-    "9 Killer Instinct":["Mike Uhlean","Jim Berger","Shawn Vitolka","Mike Zbonski Jr.","Luke Valenzuela","Doug Andrus","Jim Nevin"],
+    "9 Killer Instinct":["Mike Uhlean","Jim Berger","Shawn Vitolka","Mike Zbonski Jr.","Luke Valenzuela","Doug Andrus","Jim Nevin","Tom Nevin"],
     "10 Pete's Bunch":["Tom Schroeder","Chuck Harding","Dan Schroeder","Rob Zwart","Pete Steele","Buster Fuller","Ron Podborny","Roger Gault"]
 };
