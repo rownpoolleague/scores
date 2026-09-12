bb9af220c0b7aafd5e50c33ba8957f19d69e717a
rownpoolleague <rownpoolleague@gmail.com>
2026-09-12 04:06:34 -0500

    Automated update: scoresheetinfo.js

--- scoresheetinfo.js
+++ scoresheetinfo.js
@@ -1,8 +1,8 @@
 const scoresheetinfo = {
-    "1 Halfway Crooks":["Dan Foster","Tony Perelli","Don Young","Don Rak","Yani Marinopoulos","Tony Guthrie","Jeremie Ketner","Brooke Bridges"],
+    "1 Halfway Crooks":["Dan Foster","Tony Perelli","Don Young","Brooke Bridges","Yani Marinopoulos","Tony Guthrie","Jeremie Ketner"],
     "2 Haters Welcome":["Randy Rycraft","Joe Neal","Jennifer Wagner","Jim Czenk","Phil Nelson","Julie Ringbauer","Brett Boger","Ruth Drozdek"],
     "3 Lynn's Amigos":["Francisco Ibarra","Noe Castana","Enrique Meza","Tomas Ramirez","Ignasio Gutierrez","Jose Munoz"],
-    "4 Only Here For an Alibi":["Jim Bullock","Patti Bullock","Tito Sanchez","Jack Sedivy","Thomas Who","Joey Who"],
+    "4 Only Here For an Alibi":["Jim Bullock","Patti Bullock","Tito Sanchez","Jack Sedivy","Thomas Who","Joe Cooper"],
     "5 Dirty Ballers":["Rocky Oiler Sr.","Kevin Pattenaude","Greg Wood","Javier Mauricio","Peggy Wood","Joey Sanchez","John Schoiber","Chuck Feager"],
     "6 8 on Break":["Doc Halek","Juergen Jackert","Danny Plowman","John Amrein","Kevin Ward","Shawn Mcbride","Don Murphy","Kenny Esposito"],
     "7 Five Amigos":["Francisco Tinajero","Sergio Sanchez","Mario Orenday","Felipe Ochoa","Jeronimo Ramirez","Kevin Joyner","Tony Perez"],
